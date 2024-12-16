a79473cc4c12b8fa53deaa0e8d858446018b90d0
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2024-12-16 17:34:24 +0700

    menambahkan update di dao

--- app/src/main/java/com/example/ucplagi/data/dao/MataKuliahDAO.kt
+++ app/src/main/java/com/example/ucplagi/data/dao/MataKuliahDAO.kt
@@ -1,6 +1,7 @@
 package com.example.ucplagi.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
@@ -14,4 +15,9 @@ interface MataKuliahDAO {
     @Query("SELECT * FROM mataKuliah")
     suspend fun getAllMatakuliah(): List<MataKuliah>
 
+    @Update
+    suspend fun updateMatakuliah(mataKuliah: MataKuliah)
+
+    @Query("SELECT * FROM mataKuliah WHERE kode = :kode")
+    suspend fun getDetailMataKuliah(kode: String): MataKuliah
 }
